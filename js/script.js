d8d9538ad2f5ae8c85549584940e8e508625fe15
VLuca997 <kulertoast997@gmail.com>
2023-06-22 00:26:38 +0200

    project initialization

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*
+    - Creo una grglia con una funzione
+    - Creo un ciclo per generare le celle della griglia da 1 a 100
+    - aggiungiamo l'evento click ai div per colorare la cella selezionata
+    - stampiamo un messaggio in console con le celle cliccate.
+
+
+
+
+
+
+
+
+*/
+
+// seleziono il container
+const containerElements = document.querySelector('.my-grid-container');
+
+// funzione per generare la griglia
+
